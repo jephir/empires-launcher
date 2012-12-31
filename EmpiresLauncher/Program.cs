7564f85f241fb0592591c86d71e4427ae17ac963
Jephir <jephir@jephir.name>
2012-12-31 16:24:44 -0500

    Refactoring

--- EmpiresLauncher/Program.cs
+++ EmpiresLauncher/Program.cs
@@ -9,21 +9,32 @@ namespace EmpiresLauncher
     class Program
     {
         private const string sourceSdkBase2007Name = "source sdk base 2007";
+
         private const string sourceSdkBase2007DriveNotice = "Source SDK Base 2007 must be installed on the same drive as Empires. If it's not, remove Empires using Steam and install it again to the same drive as Source SDK Base 2007.";
+        
         private const string hl2ExeName = "hl2.exe";
+        
         private const string empiresName = "empires";
+        
         private const string installSourceSdkBase2007Uri = "steam://run/218";
 
         static void Main(string[] args)
         {
-            var argsLine = "";
+            var argsLine = JoinArgs(args);
+
+            RunEmpires(argsLine);
+        }
+
+        private static string JoinArgs(string[] args)
+        {
+            var result = "";
 
             foreach (var arg in args)
             {
-                argsLine += " " + arg;
+                result += " " + arg;
             }
 
-            RunEmpires(argsLine);
+            return result;
         }
 
         private static void RunEmpires(string args)
@@ -31,12 +42,11 @@ namespace EmpiresLauncher
             var currentDirectory = Directory.GetCurrentDirectory();
             var steamappsDirectory = Directory.GetParent(currentDirectory).Parent.FullName;
             var sourceSdkBase2007Directory = FindGameDirectory(sourceSdkBase2007Name, steamappsDirectory);
-            var sourceSdkBase2007Exists = !String.IsNullOrEmpty(sourceSdkBase2007Directory);
 
+            var sourceSdkBase2007Exists = !String.IsNullOrEmpty(sourceSdkBase2007Directory);
             if (sourceSdkBase2007Exists)
             {
                 var hl2ExeExists = DirectoryContainsFileName(sourceSdkBase2007Directory, hl2ExeName);
-
                 if (hl2ExeExists)
                 {
                     var sourceSdkBase2007Hl2Exe = Path.Combine(sourceSdkBase2007Directory, hl2ExeName);
@@ -67,7 +77,6 @@ namespace EmpiresLauncher
                 else
                 {
                     var result = MessageBox.Show("Can't start Empires because hl2.exe in Source SDK Base 2007 was not found. Click OK to run Source SDK Base 2007 and generate hl2.exe. After Source SDK Base 2007 has run, quit it, and start Empires again.\n\n" + sourceSdkBase2007DriveNotice, "Empires Mod", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
                     if (result == DialogResult.OK)
                     {
                         Process.Start(installSourceSdkBase2007Uri);
@@ -77,7 +86,6 @@ namespace EmpiresLauncher
             else
             {
                 var result = MessageBox.Show("Can't start Empires because Source SDK Base 2007 was not found. Click OK to install and run Source SDK Base 2007. After Source SDK Base 2007 has run, quit it, and start Empires again.\n\n" + sourceSdkBase2007DriveNotice, "Empires Mod", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
                 if (result == DialogResult.OK)
                 {
                     Process.Start(installSourceSdkBase2007Uri);
@@ -88,15 +96,12 @@ namespace EmpiresLauncher
         private static string FindGameDirectory(string targetGameName, string searchRoot)
         {
             var steamappDirectories = Directory.GetDirectories(searchRoot);
-
             foreach (var steamappDirectory in steamappDirectories)
             {
                 var gameDirectories = Directory.GetDirectories(steamappDirectory);
-
                 foreach (var gameDirectory in gameDirectories)
                 {
                     var gameDirectoryName = Path.GetFileName(gameDirectory);
-
                     if (gameDirectoryName.Equals(targetGameName, StringComparison.OrdinalIgnoreCase))
                     {
                         return gameDirectory;
