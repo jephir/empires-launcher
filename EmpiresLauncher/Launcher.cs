d3fe769c9a25321ceef2d7c17ecc0ca31029cab9
Jephir <jephir@jephir.name>
2013-01-03 14:37:51 -0500

    Close launcher when game signals that it has loaded

--- EmpiresLauncher/Launcher.cs
+++ EmpiresLauncher/Launcher.cs
@@ -14,10 +14,16 @@ namespace EmpiresLauncher
     public partial class Launcher : Form
     {
         private const string sourceSdkBase2007Name = "source sdk base 2007";
+
         private const string sourceSdkBase2007DriveNotice = "Source SDK Base 2007 must be installed on the same drive as Empires. If it's not, remove Empires using Steam and install it again to the same drive as Source SDK Base 2007.";
+
         private const string hl2ExeName = "hl2.exe";
+
         private const string empiresName = "empires";
+
         private const string installSourceSdkBase2007Uri = "steam://run/218";
+
+        private const string gameLoadedGuid = "{0C4BCE33-258D-4189-AE0D-B217820B7C2C}";
 
         public Launcher()
         {
@@ -25,9 +31,8 @@ namespace EmpiresLauncher
             Show();
             BringToFront();
             RunEmpires(String.Join(" ", Environment.GetCommandLineArgs()));
-            Environment.Exit(0);
         }
-        
+
         private void RunEmpires(string args)
         {
             var currentDirectory = Directory.GetCurrentDirectory();
@@ -47,27 +52,35 @@ namespace EmpiresLauncher
                     var startInfo = new ProcessStartInfo()
                     {
                         FileName = sourceSdkBase2007Hl2Exe,
-                        Arguments = launchArguments
+                        Arguments = launchArguments,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                    };
+
+                    var process = new Process()
+                    {
+                        StartInfo = startInfo
+                    };
+
+                    process.OutputDataReceived += (sender, e) =>
+                    {
+                        if (e.Data.Contains(gameLoadedGuid))
+                        {
+                            Application.Exit();
+                        }
                     };
 
                     try
                     {
-                        using (var process = Process.Start(startInfo))
-                        {
-                            new Thread(delegate()
-                            {
-                                Thread.Sleep(2000);
-                                Hide();
-                            }).Start();
-                            process.WaitForExit();
-                        }
+                        process.Start();
                     }
                     catch (Win32Exception)
                     {
                         MessageBox.Show("Can't start Empires because there was an error when running hl2.exe", "Empires Mod", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
                         throw;
                     }
+
+                    process.BeginOutputReadLine();
                 }
                 else
                 {
